07132a896be183d8007e3680287503c48d4ebb23
Nicolas Ledez <github@ledez.net>
2011-06-13 22:09:58 +0200

    Another fix 4 heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,5 +1,9 @@
-MongoMapper.connection = Mongo::Connection.new('localhost', 27017)
-MongoMapper.database = "#myapp-#{Rails.env}"
+MongoMapper.config = { 
+  Rails.env => { 'uri' => ENV['MONGOHQ_URL'] || 
+   "mongodb://localhost/#myapp-#{Rails.env}" }
+}
+
+MongoMapper.connect(Rails.env)
 
 if defined?(PhusionPassenger)
    PhusionPassenger.on_event(:starting_worker_process) do |forked|

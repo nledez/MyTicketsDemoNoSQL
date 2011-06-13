710b28e3b5a9a1e81063fca0ead3f5c97d120625
Nicolas Ledez <github@ledez.net>
2011-06-13 22:54:40 +0200

    Last fix ? For Heroku & Redis

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,2 +1,8 @@
-TRANSLATION_STORE = Redis.new  
+if ENV['REDISTOGO_URL']
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  TRANSLATION_STORE = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+else
+  TRANSLATION_STORE = Redis.new  
+end
+
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new(TRANSLATION_STORE), I18n.backend)
